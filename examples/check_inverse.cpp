2e4b29b4870d7fc66aefcf53a9efcc468138a6d1
barteksad <barteksadlej@gmail.com>
2023-03-30 19:44:28 +0200

    inverse works

--- examples/check_inverse.cpp
+++ examples/check_inverse.cpp
@@ -7,14 +7,14 @@ int main()
   // double qrel[6]{1.81, 0.99, 2.0, 1.57, -1.0, -1.0};
   // double qrel[6]{-1.0509, -2.10413, 1.73413, -2.77159, 1.0509, -3.14159};
   // double qrel[6]{0.5341, -0.6780, -0.3220, -0.0283, 0.0245, 0};
-  double qrel[6]{-3.141562,0,0,0,0,0};
-  auto [x, y, z] = forward_kinematics(qrel);
+  // double qrel[6]{-3.141562,0,0,0,0,0};
+  // auto [x, y, z] = forward_kinematics(qrel);
 
   double q[6 * 8]{0};
-  // double x, y, z;
-  // x = 0.19;
-  // y = 0.036;
-  // z = 0.77;
+  double x, y, z;
+  x = 0.4;
+  y = 0.3;
+  z = 0.3;
 
   int num_sols = inverse_kinematics(q, x, y, z);
   std::cout << "num_sols: " << num_sols << "\n";
